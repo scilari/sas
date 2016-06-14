9adbcb6016597f3fc94f20522cac416c6a5c35f8
Ilari Vallivaara <ilari.vallivaara@gmail.com>
2016-06-14 09:27:06 +0300

    Changing to non-generic to test if specialization works

--- src/main/scala/com/scilari/systematic_alias_sampling/GoldenRatioAliasSampler.scala
+++ src/main/scala/com/scilari/systematic_alias_sampling/GoldenRatioAliasSampler.scala
@@ -8,12 +8,13 @@ import scala.reflect.ClassTag
 // TODO: Use traits when inheriting from a specialized class (might lose specialization here)
 // see: http://axel22.github.io/2013/11/03/specialization-quirks.html
 //class GoldenRatioAliasSampler[VALUE_T](pmf: Array[Double], values: Array[VALUE_T])(implicit tagV: ClassTag[VALUE_T]) extends SystematicAliasSampler[VALUE_T](pmf, values){
-class GoldenRatioAliasSampler(pmf: Array[Double], values: Array[Double]) extends SystematicAliasSampler.DoubleSampler(pmf, values, new Parameters()){
+final class GoldenRatioAliasSampler(pmf: Array[Double], values: Array[Double]) extends SystematicAliasSampler.DoubleSampler(pmf, values, new Parameters()){
   type VALUE_T = Double
-  val generator = new GoldenRatioAliasSampler.GoldenRatioSequenceGenerator()
+  private[this] val generator = new GoldenRatioAliasSampler.GoldenRatioSequenceGenerator()
   private[this] val binCountAsDouble = pmf.length.toDouble
   override def sample(sampleCount: Int): Array[VALUE_T] = sampleGolden(sampleCount)
 
+  @inline
   def sampleGolden(sampleCount: Int, array: Array[VALUE_T] = null): Array[VALUE_T] = {
     val a = if(array == null) new Array[VALUE_T](sampleCount) else array
     var i = 0
@@ -30,6 +31,7 @@ object GoldenRatioAliasSampler{
   class GoldenRatioSequenceGenerator(seed: Double = Parameters.Default.random.nextDouble()) {
     private[this] var x = seed
     private[this] val gr = 0.6180339887498948482 // golden ratio conjugate
+    @inline
     def next(): Double = {
       x += gr
       x -= x.toInt
