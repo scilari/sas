ab98ad1a3098071796459a7b7d204a64bc8633c4
scilari <ilari.vallivaara@gmail.com>
2018-10-27 14:06:18 +0300

    Remove redundant method.

--- src/main/scala/com/scilari/systematic_alias_sampling/models/Sampler.scala
+++ src/main/scala/com/scilari/systematic_alias_sampling/models/Sampler.scala
@@ -9,8 +9,7 @@ trait Sampler[T] {
   implicit val classTag: ClassTag[T]
   def sample(): T
   def sample(k: Int): Array[T] = Array.fill(k)(sample())
-  def sample(output: Array[T]): Array[T] = sample(output.length, output)
-  protected def sample(k: Int, output: Array[T]) : Array[T] = {
+  def sample(output: Array[T]): Array[T] = {
     for(i <- output.indices) output(i) = sample()
     output
   }
